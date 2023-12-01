2c2d0949a2d1ef8049330e968b891dd2fd097249
Md. Iram Faiaz Khan <76048711+Faiaz98@users.noreply.github.com>
2023-12-01 18:05:59 +0600

    ransom_note.cpp

---
+++ ransom_note.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> charCount;
+        
+        for (char ch : magazine) {
+            charCount[ch]++;
+        }
+
+        for (char ch : ransomNote) {
+            if (charCount.find(ch) == charCount.end() || charCount[ch] == 0) {
+                return false;
+            }
+            charCount[ch]--;
+        }
+
+        return true;
+    }
+};

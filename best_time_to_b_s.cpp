bb78da37b0a63c95fd817fd7fe82204177aac05c
Md. Iram Faiaz Khan <76048711+Faiaz98@users.noreply.github.com>
2023-12-01 17:55:31 +0600

    best_time_to_b_s.cpp

---
+++ best_time_to_b_s.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice = numeric_limits<int>::max();
+        int maxProfit = 0;
+
+        for (int price : prices){
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+
+        return maxProfit;
+    }
+};

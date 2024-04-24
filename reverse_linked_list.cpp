e156fcc148d0e2d339ed7e04a264693c2e7c7566
Md. Iram Faiaz Khan <76048711+Faiaz98@users.noreply.github.com>
2024-04-25 01:27:01 +0600

    Update reverse_linked_list.cpp

--- reverse_linked_list.cpp
+++ reverse_linked_list.cpp
@@ -24,3 +24,47 @@ public:
         return prev;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ //interative
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *prev = nullprt;
+        ListNode *curr = head;
+
+        while (curr != nullptr) {
+            ListNode *next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+};
+
+//recursive
+class Solution {
+public:
+    ListNode* reverseListRecursive(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+
+        ListNode *newHead = reverseListRecursive(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+
+        return newHead;
+    }
+};
